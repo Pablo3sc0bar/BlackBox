d88658b79d64a16bf394025e353fd5b64f8fa3b4
Pablo3sc0bar <79912336+Pablo3sc0bar@users.noreply.github.com>
2021-12-20 19:45:50 +0300

    Базовая настройка MVC, создание базового контекста

---
+++ BlackBox/EF/Contexts/BaseContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlackBox.EF.Contexts
+{
+    public class BaseContext : DbContext
+    {
+        public BaseContext(DbContextOptions<BaseContext> options)
+               : base(options)
+        { }
+        #region DbSets
+        //public DbSet<BaseObject> BaseObjects { get; set; }
+        #endregion
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            //modelBuilder.Entity<BaseObject>().ToTable("BaseObjects");
+
+        }
+    }
+}
--- BlackBox/Startup.cs
+++ BlackBox/Startup.cs
@@ -4,18 +4,27 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Arch.EntityFrameworkCore.UnitOfWork.Collections;
+using BlackBox.EF.Contexts;
+using Arch.EntityFrameworkCore.UnitOfWork;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 namespace BlackBox
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IWebHostEnvironment env)
         {
-            Configuration = configuration;
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            builder.AddEnvironmentVariables();
+            Configuration = builder.Build();
         }
 
         public IConfiguration Configuration { get; }
@@ -24,6 +33,19 @@ namespace BlackBox
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            // установка конфигурации подключения
+            //services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+            //    .AddCookie(options => //CookieAuthenticationOptions
+            //    {
+            //        options.LoginPath = new Microsoft.AspNetCore.Http.PathString("/Account/Login");
+            //        options.AccessDeniedPath = new Microsoft.AspNetCore.Http.PathString("/Account/Login");
+            //    });
+
+            // use in memory for testing.
+            services
+                .AddDbContext<BaseContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")))
+                .AddUnitOfWork<BaseContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,10 +66,20 @@ namespace BlackBox
 
             app.UseRouting();
 
-            app.UseAuthorization();
+            //app.UseAuthentication();    // аутентификация
+            //app.UseAuthorization();     // авторизация
 
             app.UseEndpoints(endpoints =>
             {
+
+                endpoints.MapControllerRoute(
+                    name: "/",
+                    pattern: "{controller=Home}/{action=Index}/");
+
+                endpoints.MapControllerRoute(
+                    name: "Admin",
+                    pattern: "{controller=Admin}/{action=Index}/");
+
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
